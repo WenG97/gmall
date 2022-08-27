7eba481fa990c2af6b19098afcfc66d96954daea
WenG <18881931714@163.com>
2022-08-28 03:50:27 +0800

    测试代码

---
+++ service/service-product/src/test/java/com/weng/gulimall/product/TestMain.java
@@ -0,0 +1,12 @@
+package com.weng.gulimall.product;
+
+import java.util.UUID;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        // System.out.println(UUID.randomUUID());
+        // System.out.println("abcdef".substring(3));
+        System.out.println("abcdef".indexOf("a"));
+    }
+}
